720093de6223e8b5fe1144713bf16316f5265436
Erisonedu <erison.pimentao@gmail.com>
2019-09-02 07:19:38 -0300

    estrutura MVC ou começando a mplementar o sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscoreda: true,
+    underscoredAll: true,
+  }
+}
