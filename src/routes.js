da38f3ea3e8fc8d23af7aef4efeb4d00202c32c6
Erisonedu <erison.pimentao@gmail.com>
2019-09-02 05:43:31 -0300

    alterando regras de padrao do codigo para o estilo AirBNB

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/' , (req,res) => {
-  return res.json({ message : ' Hello Dev Erison '});
-})
+routes.get("/", (req, res) => {
+  return res.json({ message: " Hello Dev Erison " });
+});
 
 module.exports = routes;
